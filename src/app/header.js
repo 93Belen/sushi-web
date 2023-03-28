5d2e96f0756ed8a929fff2b0032f89d5583da2ab
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-28 08:21:48 -0500

    mobile menu appears and desappear

--- src/app/header.js
+++ src/app/header.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Gruppo } from 'next/font/google';
 import { useState } from "react";
+import Link from "next/link";
 
 
 
@@ -21,14 +22,21 @@ export default function Header(){
     const [open, setOpen] = useState(false);
     const toggleMenu = () => {
         const logo = document.getElementById('button-menu');
+        const menuDisplay = document.getElementById('menu-display');
         if(open){
             logo.style.transform = 'rotate(0deg)'
             logo.style.transitionDuration = '1s'
+            menuDisplay.style.animationName = 'desappear-menu'
+            menuDisplay.style.animationDuration = '1s'
+            setTimeout(() => {menuDisplay.style.display = 'none'}, 900)
             setOpen(() => false)
         }
         else {
             logo.style.transform = 'rotate(270deg)'
             logo.style.transitionDuration = '1s'
+            menuDisplay.style.animationName = 'appear-menu'
+            menuDisplay.style.animationDuration = '0.9s'
+             menuDisplay.style.display = 'flex'
             setOpen(() => true)
         }
         
@@ -40,12 +48,17 @@ export default function Header(){
                 <Image className='h-full w-auto' width={100} src={require('../../imgs/logo.png')} />
                 <h1 className={`text-mygreen ${h1font.variable} align-baseline md:text-3xl text-lg mt-auto`}>Sushi House</h1>
             </div>
-            <nav className='w-3/6 h-full justify-items-end pr-6 pt-3 box-border'>
+            <nav className='w-3/6 h-full justify-items-end pr-6 pt-3 box-border md:hidden'>
                 <div onClick={toggleMenu} className='w-fit ml-auto box-border'>
                 <svg id='button-menu' width="50" height="50" viewBox="0 0 28 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <line x1="2.5" y1="2.5" x2="25.5" y2="2.5" stroke="#E31837" stroke-width="5" stroke-linecap="round"/>
                     <line x1="2.5" y1="11.5" x2="25.5" y2="11.5" stroke="#E31837" stroke-width="5" stroke-linecap="round"/>
                 </svg>
+                <section id='menu-display' className='w-screen h-screen absolute top-[10vh] left-0 flex-col justify-evenly justify-items-center box-border pb-10 hidden'>
+                    <Link className='m-auto text-2xl' href='/'>Home</Link>
+                    <Link className='m-auto text-2xl' href='/'>Guides</Link>
+                    <Link className='m-auto text-2xl' href='/'>Our Story</Link>
+                </section>
                 </div>
             </nav>
             <nav className='border-2 border-white w-3/6 h-full hidden'>
