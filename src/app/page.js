187a157881f2034e8b1156a8f092e0a65b75737c
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-28 09:45:17 -0500

    fixed home page grid

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
 export default function Home() {
   return (
-    <main className='flex flex-row justify-around min-h-[90vh]'>
-      <section className='border-2 border-white w-full'>
-        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+    <main className='flex flex-row justify-around min-h-[90vh] box-border'>
+      <section className='border-2 border-white w-full flex flex-col md:grid md:grid-cols-2 md:grid-rows-2 gap-10 p-10 justify-items-center align-middle'>
+        <div className='border-2 border-myred h-3/6 w-full'>
           
         </div>
-        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+        <div className='border-2 border-myred h-3/6 w-full'>
           
         </div>
-        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+        <div className='border-2 border-myred h-3/6 w-full'>
           
         </div>
-        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+        <div className='border-2 border-myred h-3/6 w-full'>
           
         </div>
       </section>
-      <section className='hidden md:block border-white border-2 w-full'>
-      <svg width="178" height="274" viewBox="0 0 178 274" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <section className='hidden md:block border-white border-2 w-full justify-items-center align-middle p-2 content-center'>
+      <svg className='m-auto h-full' width="50%" height="auto" viewBox="0 0 178 274" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M116.674 251.167C115.967 251.339 115.254 250.911 115.073 250.206L54.5436 14.644C53.9281 12.2485 55.3903 9.81214 57.7938 9.22858C60.1955 8.64548 62.6106 10.1373 63.1643 12.5459L117.655 249.578C117.818 250.287 117.381 250.995 116.674 251.167Z" fill="#2D2626" stroke="#F6F2F6"/>
         <path d="M62.7345 48.5484L62.0634 45.9355C62.0634 45.9355 64.3535 44.6136 66.2833 44.145C68.213 43.6765 70.8661 43.7742 70.8661 43.7742L71.1611 45.1146L71.4705 46.4454C68.3775 46.327 65.6709 46.992 62.7345 48.5484Z" fill="#E31837"/>
         <path d="M172.216 242.236C171.655 242.699 170.827 242.625 170.357 242.07L13.0537 56.5732C11.454 54.6868 11.7058 51.8566 13.6133 50.2822C15.5194 48.709 18.3435 48.9967 19.8933 50.9218L172.405 240.378C172.861 240.945 172.777 241.773 172.216 242.236Z" fill="#2D2626" stroke="#F6F2F6"/>
