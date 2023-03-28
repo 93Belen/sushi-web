c448496b15f825613093053be8a1d4f2ed3abb6a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-28 09:26:15 -0500

    defined layout of main home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
-    <main>
-      
+    <main className='flex flex-row justify-around min-h-[90vh]'>
+      <section className='border-2 border-white w-full'>
+        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+          
+        </div>
+        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+          
+        </div>
+        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+          
+        </div>
+        <div className='border-2 border-myred h-[200xp] w-[200px]'>
+          
+        </div>
+      </section>
+      <section className='hidden md:block border-white border-2 w-full'>
+      <svg width="178" height="274" viewBox="0 0 178 274" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M116.674 251.167C115.967 251.339 115.254 250.911 115.073 250.206L54.5436 14.644C53.9281 12.2485 55.3903 9.81214 57.7938 9.22858C60.1955 8.64548 62.6106 10.1373 63.1643 12.5459L117.655 249.578C117.818 250.287 117.381 250.995 116.674 251.167Z" fill="#2D2626" stroke="#F6F2F6"/>
+        <path d="M62.7345 48.5484L62.0634 45.9355C62.0634 45.9355 64.3535 44.6136 66.2833 44.145C68.213 43.6765 70.8661 43.7742 70.8661 43.7742L71.1611 45.1146L71.4705 46.4454C68.3775 46.327 65.6709 46.992 62.7345 48.5484Z" fill="#E31837"/>
+        <path d="M172.216 242.236C171.655 242.699 170.827 242.625 170.357 242.07L13.0537 56.5732C11.454 54.6868 11.7058 51.8566 13.6133 50.2822C15.5194 48.709 18.3435 48.9967 19.8933 50.9218L172.405 240.378C172.861 240.945 172.777 241.773 172.216 242.236Z" fill="#2D2626" stroke="#F6F2F6"/>
+        <path d="M35.2254 83.4993L33.4808 81.4416C33.4808 81.4416 34.9639 79.2524 36.4955 77.9884C38.027 76.7244 40.4565 75.6539 40.4565 75.6539L41.3072 76.731L42.1666 77.7931C39.3322 79.0371 37.1875 80.8171 35.2254 83.4993Z" fill="#E31837"/>
+      </svg>
+      </section>
     </main>
   )
 }
