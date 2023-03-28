52661bf7936af2d72c256ff438f5410647311a5c
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-28 18:18:11 -0500

    fix links

--- src/app/oakpark/layout.js
+++ src/app/oakpark/layout.js
@@ -10,15 +10,15 @@ export default function Layout({children}) {
     <main className='justify-around min-h-[85vh] box-border text-center'>
     <h1 className='text-3xl neonText'>Oak Park</h1>
     <div className='hidden md:flex flex-row justify-evenly  h-[10vh] w-full text-center'>
-            <Link className='m-auto hover:text-mygreen' href='/guides'>Oak Park Favorites</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/ingridients'>Appetizers</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/sake'>Soups & Salads</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides'>Sushi Dinner</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/ingridients'>Entrees</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/sake'>Noodles</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides'>Kids</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/ingridients'>Desserts</Link>
-            <Link className='m-auto hover:text-mygreen' href='/guides/sake'>Party Trays</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Oak Park Favorites</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Appetizers</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Soups & Salads</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Sushi Dinner</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Entrees</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Noodles</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Kids</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Desserts</Link>
+            <Link className='m-auto hover:text-mygreen' href='/'>Party Trays</Link>
     </div>
     {children}
     </main>
