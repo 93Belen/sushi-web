7cf3b21526919a3a6fe031ebc75ecd22868e39b5
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-04-02 10:44:50 -0500

    added menu to thrid location

--- src/app/oakpark/layout.js
+++ src/app/oakpark/layout.js
@@ -11,15 +11,13 @@ export default function Layout({children}) {
     <main className='justify-around min-h-[85vh] box-border text-center text-mywhite'>
     <h1 className='text-3xl neonText'>Oak Park</h1>
     <div className='hidden md:flex flex-row justify-evenly  h-[10vh] w-full text-center'>
-            <Link className='m-auto hover:text-mygreen' href='/'>Oak Park Favorites</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Appetizers</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Soups & Salads</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Sushi Dinner</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Entrees</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Noodles</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Kids</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Desserts</Link>
-            <Link className='m-auto hover:text-mygreen' href='/'>Party Trays</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/favorites'>Oak Park Favorites</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/appetizers'>Appetizers</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/soups_salads'>Soups & Salads</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/sushi_dinner'>Sushi Dinner</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/entrees'>Entrees</Link>
+            <Link className='m-auto hover:text-mygreen text-mywhite' href='/oakpark/noodles'>Noodles</Link>
+
     </div>
     {children}
     </main>
---
+++ src/app/oakpark/soups_salads/page.js
@@ -0,0 +1,30 @@
+"use client"
+
+import { selectMenu } from "components/app/redux/selectors"
+import { useSelector } from "react-redux"
+
+export default function Page(){
+    const menu = useSelector(selectMenu)
+    const ss = menu.leawood.soups_salads;
+    const displayCards = () => {
+        const arr = [];
+        ss.forEach(element => {
+            arr.push(
+                <div className='p-4 border-2 border-mywhite rounded-xl m-4'>
+                    <p className='text-mygreen'>{element.name}</p>
+                    <p className='text-left'>{element.description}</p>
+                </div>
+            )
+        });
+        return arr;
+    }
+
+
+
+    return(
+        <div className='w-full grid grid-cols-3 grid-row-2 gap-6'>
+            {displayCards()}
+        </div>
+    )
+
+}
