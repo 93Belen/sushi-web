63b114ad4d06845e3ac83681669a53fc60908e6e
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-04-02 11:10:29 -0500

    trying to fix deployment error

--- src/app/redux/menu.js
+++ src/app/redux/menu.js
@@ -3,7 +3,40 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const options = {
     name: 'menu',
-    initialState: {},
+    initialState: {
+        leawood: {
+            favorites: [],
+            appetizers: [],
+            soups_salads: [],
+            sushi_dinner: [],
+            entrees: [],
+            noodles: []
+        },
+        downes_grove: {
+            favorites: [],
+            appetizers: [],
+            soups_salads: [],
+            sushi_dinner: [],
+            entrees: [],
+            noodles: []
+        },
+        oak_park: {
+            favorites: [],
+            appetizers: [],
+            soups_salads: [],
+            sushi_dinner: [],
+            entrees: [],
+            noodles: []
+        },
+        westmont: {
+            favorites: [],
+            appetizers: [],
+            soups_salads: [],
+            sushi_dinner: [],
+            entrees: [],
+            noodles: []
+        }
+    },
     reducers: {
         changeMenu: (state, action) => {
             state = action.payload
